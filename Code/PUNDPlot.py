9d3afc1b3c85d6123f600b7f3081e6b75d945c53
Theodor Blom <theodor.blom1@gmail.com>
2021-03-02 17:52:52 +0100

    Started looking at PUND code. Can now clean the data and plot a simple PUND plot.

---
+++ Code/PUNDPlot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+### USAGE ###
+# ./PUNDPlot.py <Path to clean PUND data>
+
+import sys
+import matplotlib.pyplot as plt
+
+## Read data
+
+PUNDpath = sys.argv[1]
+PUNDfile = open(PUNDpath)
+PUNDfilelines = PUNDfile.readlines()
+
+PUNDTime = []
+PUNDCurrent = []
+PUNDVoltage = []
+for line in PUNDfilelines:
+    PUNDTime.append(float(line.split(",")[1]))
+    PUNDCurrent.append(float(line.split(",")[2]))
+    PUNDVoltage.append(float(line.split(",")[3]))
+
+PUNDfile.close()
+
+fig = plt.figure(figsize = (9,6))
+ax1 = fig.add_subplot(111)
+ax2 = ax1.twinx()
+
+ax1.plot([t * 10**3 for t in PUNDTime], [i * 10**6 for i in PUNDCurrent], label = "Current", color = "b")
+
+ax2.plot([t * 10**3 for t in PUNDTime], PUNDVoltage, label = "Voltage", color = "c")
+
+fig.legend(fontsize = 'x-large', loc = 4, bbox_to_anchor = (1,0), bbox_transform = ax1.transAxes)
+
+ax1.tick_params('both', labelsize = "x-large")
+ax2.tick_params('both', labelsize = "x-large")
+
+ax1.set_xlabel("Time [ms]", fontsize = "xx-large")
+ax1.set_ylabel("Current [uA]", fontsize = "xx-large")
+ax2.set_ylabel("Voltage [V]", fontsize = "xx-large")
+
+#plt.savefig('../Fig/PLE_Y.png')
+plt.show()
+
+
