469c719d333bca341e0b73c65d2175862c4b11b6
Theodor Blom <theodor.blom1@gmail.com>
2021-05-17 13:36:49 +0200

    Can now plot multiple FreqDisp in same figure through manual entry. Must rewrite most code using functions!

--- Code/CapacitancePlot.py
+++ Code/CapacitancePlot.py
@@ -21,53 +21,55 @@ Filter1 = sys.argv[3] if len(sys.argv) > 3 else ''
 Filter2 = sys.argv[4] if len(sys.argv) > 4 else ''
 
 ## Read data
+def PlotCV(ax1, DataArray):
+    CVPath = DataArray[0]
+    Filter1 = DataArray[1] if len(DataArray) > 1 else ''
+    Filter2 = DataArray[2] if len(DataArray) > 2 else ''
+    CVFiles = os.listdir(CVPath)
+    CVFiles.sort()
 
-CVPath = sys.argv[1]
-CVFiles = os.listdir(CVPath)
-CVFiles.sort()
+    CVVoltage = []
+    freq = []
+    C_HZO = []
+    epsilon_r = []
 
-CVVoltage = []
-freq = []
-C_HZO = []
-epsilon_r = []
+    i = 0
+    for Cfile in CVFiles:
+        if Cfile.endswith('.csv') and Cfile.find(Filter1) != -1 and Cfile.find(Filter2) != -1: #find filters to Filter
+            CVFile = open(os.path.join(CVPath, Cfile), 'r')
+            CVFilelines = CVFile.readlines()
+            V_max = Cfile.split("_")[6]
+            SampleID = Cfile.split("_")[2] + "_" + Cfile.split("_")[3] + "_" + Cfile.split("_")[4] + "_" + Cfile.split("_")[1]
 
-i = 0
-for Cfile in CVFiles:
-    if Cfile.endswith('.csv') and Cfile.find(Filter1) != -1 and Cfile.find(Filter2) != -1: #find filters to Filter
-        CVFile = open(os.path.join(CVPath, Cfile), 'r')
-        CVFilelines = CVFile.readlines()
-        V_max = Cfile.split("_")[6]
-        SampleID = Cfile.split("_")[2] + "_" + Cfile.split("_")[3] + "_" + Cfile.split("_")[4] + "_" + Cfile.split("_")[1]
+            CVVoltage.append([float(line.split(",")[2]) for line in CVFilelines])
+            CVX = [float(line.split(",")[1]) * -1 for line in CVFilelines]
 
-        CVVoltage.append([float(line.split(",")[2]) for line in CVFilelines])
-        CVX = [float(line.split(",")[1]) * -1 for line in CVFilelines]
+            CVFile.close()
 
-        CVFile.close()
+            r = float(Cfile.split("_")[5].replace('um','')) * 10**-6
+            A = pi * r**2
+            freq.append(float(Cfile.split("_")[7].replace('Hz','')))
 
-        r = float(Cfile.split("_")[5].replace('um','')) * 10**-6
-        A = pi * r**2
-        freq.append(float(Cfile.split("_")[7].replace('Hz','')))
+            C_HZO.append(np.array([1 / (2 * pi * freq[i] * X * A) for X in CVX]))
+            epsilon_r.append(np.array([(d / epsilon_0) * c for c in C_HZO[i]]))
 
-        C_HZO.append(np.array([1 / (2 * pi * freq[i] * X * A) for X in CVX]))
-        epsilon_r.append(np.array([(d / epsilon_0) * c for c in C_HZO[i]]))
+            i += 1
 
-        i += 1
+    if i == 0:
+        print("Didn't find any matching data for %s with specified filter. Exiting..."%CVPath)
+        exit(1)
 
-if i == 0:
-    print("Didn't find any matching data. Exiting...")
-    exit(1)
+    Cdisp = [(((C_HZO[0::2][-1][-1] - C_HZO[0::2][j][-1])/C_HZO[0::2][-1][-1]) * 10**2)/math.log10(freq[0::2][j]/freq[-1]) for j in range(int(i/2))]
+    """
+    ## Plot Capacitance and Epsilon
+    fig = plt.figure(figsize = (10,6))
+    ax1 = fig.add_subplot(111)
+    ax2 = ax1.twinx()
 
-Cdisp = [(((C_HZO[0::2][-1][-1] - C_HZO[0::2][j][-1])/C_HZO[0::2][-1][-1]) * 10**2)/math.log10(freq[0::2][j]/freq[-1]) for j in range(int(i/2))]
-"""
-## Plot Capacitance and Epsilon
-fig = plt.figure(figsize = (10,6))
-ax1 = fig.add_subplot(111)
-ax2 = ax1.twinx()
+    cmap = plt.get_cmap('Set1')
+    colors = iter(cmap(np.linspace(0,1,9)))
 
-cmap = plt.get_cmap('Set1')
-colors = iter(cmap(np.linspace(0,1,9)))
-
-for i in range(i):
+    for i in range(i):
     if i % 2 == 0:
         color = next(colors)
         ax1.plot(CVVoltage[i], [c * 10**2 for c in C_HZO[i]], label = '%s MHz'%int(freq[i] * 10**-6) if freq[i] % 1000000 == 0 else '%s kHz'%int(freq[i] * 10**-3), color = color)
@@ -78,37 +80,37 @@ for i in range(i):
 
         ax2.plot(CVVoltage[i], epsilon_r[i], color = "r", alpha = 0)
 
-ax2.annotate("", (-0.1, np.min(epsilon_r)), (-1, np.min(epsilon_r)), arrowprops = dict(arrowstyle = "->", linewidth = 1.5))
-ax2.annotate("", (0.1, np.min(epsilon_r)), (1, np.min(epsilon_r)), arrowprops = dict(arrowstyle = "->", linestyle = '--', linewidth = 1.5))
+    ax2.annotate("", (-0.1, np.min(epsilon_r)), (-1, np.min(epsilon_r)), arrowprops = dict(arrowstyle = "->", linewidth = 1.5))
+    ax2.annotate("", (0.1, np.min(epsilon_r)), (1, np.min(epsilon_r)), arrowprops = dict(arrowstyle = "->", linestyle = '--', linewidth = 1.5))
 
-fig.legend(fontsize = 'x-large', loc = 2, bbox_to_anchor = (0,1), bbox_transform = ax1.transAxes)
+    fig.legend(fontsize = 'x-large', loc = 2, bbox_to_anchor = (0,1), bbox_transform = ax1.transAxes)
 
-plt.title(SampleID)
+    plt.title(SampleID)
 
-ax1.tick_params('both', labelsize = "x-large")
-ax2.tick_params('both', labelsize = "x-large")
+    ax1.tick_params('both', labelsize = "x-large")
+    ax2.tick_params('both', labelsize = "x-large")
 
-ax1.set_xlabel("Voltage [V]", fontsize = "xx-large")
-ax1.set_ylabel("Capacitance [$\mu$F/cm²]", fontsize = "xx-large")
-ax2.set_ylabel("Dielectric Constant $\epsilon_r$", fontsize = "xx-large")
+    ax1.set_xlabel("Voltage [V]", fontsize = "xx-large")
+    ax1.set_ylabel("Capacitance [$\mu$F/cm²]", fontsize = "xx-large")
+    ax2.set_ylabel("Dielectric Constant $\epsilon_r$", fontsize = "xx-large")
 
-#plt.savefig('../Fig/InAs%s'%Cfile.split("_")[2] + '/CV_%s.png'%SampleID)
-"""
-## Plot Frequency-Dispersion
-#fig = plt.figure(figsize = (10,6))
-#ax1 = fig.add_subplot(111)
+    #plt.savefig('../Fig/InAs%s'%Cfile.split("_")[2] + '/CV_%s.png'%SampleID)
+    """
+    ## Plot Frequency-Dispersion
+    #fig = plt.figure(figsize = (10,6))
+    #ax1 = fig.add_subplot(111)
 
-ax1.semilogx(freq[0::2], Cdisp, marker = 'o', markersize = 15, ls = ':')
-ax1.set_ylim([0, max(Cdisp) + 1])
+    ax1.semilogx(freq[0::2], Cdisp, marker = 'o', markersize = 15, ls = ':', label = SampleID)
+    #ax1.set_ylim([0, max(Cdisp) + 1])
 
-plt.title(SampleID)
+    #plt.title(SampleID)
 
-ax1.tick_params('both', labelsize = "x-large")
-ax1.set_xlabel("Frequency [Hz]", fontsize = "xx-large")
-ax1.set_ylabel('Frequency Dispersion @ +%s'%V_max + ' [%/dec]', fontsize = "xx-large")
+    ax1.tick_params('both', labelsize = "x-large")
+    ax1.set_xlabel("Frequency [Hz]", fontsize = "xx-large")
+    ax1.set_ylabel('Frequency Dispersion @ +%s'%V_max + ' [%/dec]', fontsize = "xx-large")
 
-#plt.savefig('../Fig/InAs%s'%Cfile.split("_")[2] + '/FreqDisp_%s.png'%SampleID)
+    #plt.savefig('../Fig/InAs%s'%Cfile.split("_")[2] + '/FreqDisp_%s.png'%SampleID)
 
-plt.show()
+    #plt.show()
 
 
--- Code/test.py
+++ Code/test.py
@@ -7,5 +7,23 @@ import CapacitancePlot
 fig = plt.figure(figsize = (10,6))
 ax1 = fig.add_subplot(111)
 
-CapacitancePlot("../Data/InAsFlashIntC/CV/1/D1/")
+dataLoc = "../Data/InAsFlashIntC/CV/"
 
+dataToPlot = [ 
+        ["%s1/C12/"%dataLoc, "2.5V"],
+        ["%s1/D13/"%dataLoc, "2.5V"],
+        ["%s1/D2/"%dataLoc, "293K", "2.5V"],
+        ["%s1/D2/"%dataLoc, "13K", "2.5V"],
+        #["%s1/E5/"%dataLoc, "3V"],
+        #["%s1/D2/"%dataLoc, "293K", "3V"],
+        #["%s1/D3/"%dataLoc, "3V"],
+        #["%s1/E12/"%dataLoc],
+        #["%s1/E2/"%dataLoc, "13K", "3V"],
+        ["%s1/D3/"%dataLoc, "2.5V"]
+        ]
+
+for data in dataToPlot:
+    CapacitancePlot.PlotCV(ax1, data)
+
+ax1.legend(fontsize = 10, loc = 1, bbox_to_anchor = (1,1), bbox_transform = ax1.transAxes)
+plt.show()
