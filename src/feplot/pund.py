516d23963b1f9c4ce785d88af59fb298069186b3
Theodor Blom <me@theodorblom.com>
2022-07-11 22:33:02 +0200

    Added get_data function

--- src/feplot/pund.py
+++ src/feplot/pund.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
 
+def get_data(path: str) -> tuple[list[float], ...]:
+    data = ([], [], [])
+    with open(path, 'r') as file:
+        for line in file.readlines():
+            data[0].append(float(line.split(",")[1]))
+            data[1].append(float(line.split(",")[2]))
+            data[2].append(float(line.split(",")[3]))
+
+    return data
+
 def main(paths: tuple[str, ...]) -> None:
-    pass
+
+    for path in paths:
+        pund_data: tuple[list[float], ...] = get_data(path) 
